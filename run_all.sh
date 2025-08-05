c3c61b0d04e00a63252ced12b79963964d906dc5
govindchari <govindchari1@gmail.com>
2025-08-04 17:07:38 -0700

    New results

--- run_all.sh
+++ run_all.sh
@@ -2,6 +2,12 @@
 
 set -e  # Exit immediately if a command exits with a non-zero status
 
+echo "Running Maros benchmark..."
+python run_maros.py
+
+echo "Running SuiteSparse benchmark..."
+python run_suitesparse.py
+
 echo "Generating benchmark solvers..."
 python generate_benchmark_solvers.py
 
@@ -14,22 +20,16 @@ python generate_mpc_solvers.py
 echo "Running mpc problems..."
 python run_mpc.py
 
-echo "Running Maros benchmark..."
-python run_maros.py
+echo "Analyzing Maros results..."
+python analyze_maros.py
 
-echo "Running SuiteSparse benchmark..."
-python run_suitesparse.py
+echo "Analyzing SuiteSparse results..."
+python analyze_suitesparse.py
 
 echo "Analyzing benchmark problems..."
 python analyze_benchmarks.py
 
 echo "Analyzing mpc results..."
 python analyze_mpc.py
-
-echo "Analyzing Maros results..."
-python analyze_maros.py
-
-echo "Analyzing SuiteSparse results..."
-python analyze_suitesparse.py
 
 echo "All tasks completed successfully."
